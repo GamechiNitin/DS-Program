db3eed95e393d61adf79c2781c394eab9f8d0a9b
GamechiNitin <nitingamechi1696@gmail.com>
2023-06-20 22:37:40 +0530

    Loops : Do While Loop

---
+++ concept/03_loops/do_while/do_while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin >> n;
+    
+    cout << "Do While : " << endl ;
+    do{
+        cout << n << " ";
+    } while (n--);
+}
