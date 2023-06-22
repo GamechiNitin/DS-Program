8b8608cd76d95c674ec91979fa8119515b6eb678
GamechiNitin <nitingamechi1696@gmail.com>
2023-06-22 08:46:23 +0530

    C++ : Pattern Ex.5

---
+++ tutorial/patterns/p5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /* 
+        1   2   3
+        4   5   6   
+        7   8   9
+    */
+    int n;
+    cin >> n;
+    int i = 1;
+    int count  = 0;
+
+    while (i <= n){
+            int j = 1;
+            while (j <= n){
+            count += 1;
+            cout<< count << " ";
+            j++;
+
+        }
+        cout << endl;
+        i++;
+    }
+}
