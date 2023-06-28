ec4a6ee58c2769ed14eee6173d37f71757e02bd5
GamechiNitin <nitingamechi1696@gmail.com>
2023-06-28 09:01:34 +0530

    C++ : Pattern Ex.21

---
+++ tutorial/patterns/p21.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+            1
+           22
+          333
+         4444
+    */
+    int n;
+    cin >> n;
+    int i = 1;
+
+    while (i<=n){
+
+        int space = n-i;
+        while (space)
+        {
+            cout << " ";
+            space--;
+
+        }
+
+        int j = 1;
+        while (j <= i){
+            cout << i;
+            j++;
+            }
+            cout << endl;
+        i++;
+
+        
+        
+    }
+    
+}
