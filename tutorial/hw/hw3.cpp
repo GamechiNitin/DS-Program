c4cbf17d44328d6daf9c17dbf5dfce2c583bdf50
GamechiNitin <nitingamechi1696@gmail.com>
2023-06-30 08:10:20 +0530

    Hw 3

---
+++ tutorial/hw/hw3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 1;
+    int b = 2;
+    if(a-- > 0 || ++b > 2){
+        cout << "S1 : Inside if Condition"<< endl;
+    }
+    else {
+        cout << "S1 : Inside if Condition";
+    }
+        cout << a << " " << b << endl;
+/* o/p : If condition : a = 0; b = 2;
+  Note : || Or doesn't Evalute 2 condition if first is true.
+*/
+}
