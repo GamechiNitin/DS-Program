c1652795b65f84cfe018b3fd1cc6f8540ceb6122
GamechiNitin <nitingamechi1696@gmail.com>
2023-07-02 21:26:30 +0530

    Hw 9

---
+++ tutorial/hw/hw9.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i<5;i++){
+        for (int j = 0; j < 5; j++){
+        cout << i  << j << " ";
+        }
+    }
+}
+// O/p : 00 01 02 03 04 10 11 12 13 14 15 .... 43 44
