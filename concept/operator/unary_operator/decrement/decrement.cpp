1e285c960522ad1b27dce7e9fb7f9f9106ab8190
GamechiNitin <nitingamechi1696@gmail.com>
2023-07-03 21:32:25 +0530

    C++ : Decrement operator

---
+++ concept/operator/unary_operator/decrement/decrement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Post Decrement" << endl;
+    int i = 10;
+    cout << i-- << endl;
+    cout << i << endl;
+    
+    cout << "Pre Decrement" << endl;
+    int j = 10;
+    cout << --j << endl;
+    cout << j << endl;
+}
