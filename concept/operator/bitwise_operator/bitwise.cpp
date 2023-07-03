25cfea6f87580ffb60b10cfb6db59ffb3f445b20
GamechiNitin <nitingamechi1696@gmail.com>
2023-07-03 21:29:41 +0530

    C++ : Bitwise operator

---
+++ concept/operator/bitwise_operator/bitwise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4;
+    int b= 6;
+
+    cout << "AND Operator & : " << (a&b) << endl;
+    cout << "OR Operator | : " << (a|b) << endl;
+    cout << "NOT Operator ~ : " << ~a << endl;
+    cout << "XOR Operator ^ : " << (a^b) << endl;
+}
