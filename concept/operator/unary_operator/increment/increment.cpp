0761c9ea6b3d03cd54e403c0ab6919f80d53b788
GamechiNitin <nitingamechi1696@gmail.com>
2023-07-03 21:31:59 +0530

    C++ : Increment operator

---
+++ concept/operator/unary_operator/increment/increment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Post Increment" << endl;
+    int i = 10;
+    cout << i++ << endl;
+    cout << i << endl;
+    
+    cout << "Pre Increment" << endl;
+    int j = 10;
+    cout << ++j << endl;
+    cout << j << endl;
+}
