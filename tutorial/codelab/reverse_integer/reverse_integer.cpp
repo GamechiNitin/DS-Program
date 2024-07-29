4d84bbbe935b0ebdda314aa523cf1330aca53387
GamechiNitin <nitingamechi1696@gmail.com>
2024-07-29 18:05:28 +0530

    Q : Reverse Integer

---
+++ tutorial/codelab/reverse_integer/reverse_integer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ /*
+    Q : Reverse Integer
+    Input : 321
+    Output : 123
+
+    Logic - Ans = (ans * 10) + digit
+ */
+
+int n;
+cin >>n;
+
+int ans = 0;
+while(n%10 != 0){
+    int digit = n%10;
+    ans = (ans * 10) + digit;
+
+    n = n/10;
+    cout <<  digit << ' '<< ans << endl;
+}
+    cout <<  ans << endl;
+    return 0;
+}
---
+++ tutorial/codelab/reverse_integer/reverse_integer.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  /*
+    Q : Reverse Integer
+    Input : 321
+    Output : 123
+
+    Logic - Ans = (ans * 10) + digit
+ */
+
+  int n = int.parse(stdin.readLineSync()!);
+  int ans = 0;
+
+  while (n % 10 != 0) {
+    int digit = n % 10;
+    ans = (ans * 10) + digit;
+
+    n = n ~/ 10;
+  }
+  print(ans);
+}
