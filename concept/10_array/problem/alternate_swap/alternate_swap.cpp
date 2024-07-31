f16d808dce4b88c30b0a1e86c0ed1d206e4b0af0
GamechiNitin <nitingamechi1696@gmail.com>
2024-07-31 16:43:21 +0530

    Array : Alternate array : Odd/Even length

--- concept/10_array/problem/alternate_swap/alternate_swap.cpp
+++ concept/10_array/problem/alternate_swap/alternate_swap.cpp
@@ -3,15 +3,30 @@ using namespace std;
 
 
 
-int main(){
-
-    int arr[6] = {54,56,7,25,5,89};
-
-    for (int i = 0; i < 6; i=i+2){
-        swap(arr[i],arr[i+1]);
+void change(int arr[], int size){
+     for (int i = 0; i < size; i=i+2){
+        if( (i+1) < size ){
+            swap(arr[i], arr[i+1]);
+        }
     }
 
+}
+int main(){
+
+    int even[6] = {54,56,7,25,5,89};
+    int odd[5] = {4,6,7,2,5};
+
+   change(even,6);
+   change(odd,5);
+
     for (int i = 0; i < 6; i++){
-       cout<< arr[i] << " ";
+       cout<< even[i] << " ";
+    } 
+
+       cout<< endl;
+      for (int i = 0; i < 5; i++){
+       cout<< odd[i] << " ";
     } 
 }
+
+
--- concept/10_array/problem/alternate_swap/alternate_swap.dart
+++ concept/10_array/problem/alternate_swap/alternate_swap.dart
@@ -1,5 +1,6 @@
 void main() {
   List<int> data = [5, 4, 3, 7, 9, 5];
+  List<int> odd = [4, 54, 33, 77, 49];
   print(data);
 
   for (var i = 0; i < data.length; i = i + 2) {
@@ -14,6 +15,19 @@ void main() {
   }
 
   print(data);
-}
 
-void swap(int a, int b) => b = a;
+  for (var i = 0; i < odd.length; i = i + 2) {
+    if (i + 1 < odd.length) {
+      int a = odd[i];
+      int b = odd[i + 1];
+      int x = i;
+      int y = i + 1;
+      odd.removeAt(x);
+      odd.insert(x, b);
+      odd.removeAt(y);
+      odd.insert(y, a);
+    }
+  }
+
+  print(odd);
+}
