2b71ec8dddeb1c135d27eb4b8bce3433410db1be
GamechiNitin <nitingamechi1696@gmail.com>
2024-08-04 22:39:50 +0530

    Array : Binary : Book Allocation - Dart

---
+++ concept/10_array/binary_search/book_allocation/book_allocation.dart
@@ -0,0 +1,46 @@
+void main() {
+  int student = 2;
+  List<int> pages = [10, 20, 30, 40];
+
+  int result = book(pages, student);
+  print(result);
+}
+
+int book(List<int> pages, int student) {
+  int start = 0;
+  int end = pages.fold(0, (previousValue, element) => previousValue + element);
+  int ans = -1;
+  int mid = start + (end - start) ~/ 2;
+
+  while (start <= end) {
+    if (isPossible(pages, student, mid)) {
+      ans = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+
+    mid = start + (end - start) ~/ 2;
+  }
+
+  return ans;
+}
+
+bool isPossible(List<int> pages, int student, int mid) {
+  int studentCount = 1;
+  int pageSum = 0;
+
+  for (var i = 0; i < pages.length; i++) {
+    if (pageSum + pages[i] <= mid) {
+      pageSum += pages[i];
+    } else {
+      studentCount++;
+      if (studentCount > student || pages[i] > mid) {
+        return false;
+      }
+      pageSum = pages[i];
+    }
+  }
+
+  return true;
+}
