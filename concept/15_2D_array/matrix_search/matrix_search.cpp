0cb117518b5f036dc146842ab1347a8d9aabcc1a
GamechiNitin <nitingamechi1696@gmail.com>
2024-08-31 22:54:37 +0530

    2D Array : Matrix Search

---
+++ concept/15_2D_array/matrix_search/matrix_search.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4][5] = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 32},
+        {10, 13, 14, 17, 24},
+    };
+
+    int row = 4;
+    int col = 5;
+
+    int rowIndex = 0;
+    int colIndex = col - 1;
+
+    int target = 5;
+    bool isPresent = false;
+
+    while (rowIndex < row && colIndex >= 0)
+    {
+        int element = arr[rowIndex][colIndex];
+
+        if (element == target)
+        {
+            isPresent = true;
+            break;
+        }
+
+        if (element < target)
+        {
+            rowIndex++;
+        }
+        else
+        {
+            colIndex--;
+        }
+    }
+
+    if (isPresent)
+    {
+        cout << "Present" << endl;
+    }
+    else
+    {
+        cout << "Not Present" << endl;
+    }
+}
